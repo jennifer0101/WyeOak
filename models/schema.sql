0ed3711e6779cab9e2f1e883abce66cdc15698aa
connander <connorpstark@gmail.com>
2019-10-22 20:31:53 -0500

    files run upon node startup, make sure port is the same as the one you use on mysql

--- models/schema.sql
+++ models/schema.sql
@@ -2,13 +2,13 @@ DROP DATABASE IF EXISTS wyeoak;
 CREATE DATABASE wyeoak;
 
 
--- USE wyeoak;
+ USE wyeoak;
 
--- CREATE table destinations(
--- id INT NOT NULL AUTO_INCREMENT,
--- address VARCHAR(30), 
--- city VARCHAR(30) NOT NULL,
--- state VARCHAR(30) NOT NULL,
--- zip INTEGER(10),
--- PRIMARY KEY(id)
--- );
+ CREATE table destinations(
+ id INT NOT NULL AUTO_INCREMENT,
+ address VARCHAR(30), 
+ city VARCHAR(30) NOT NULL,
+ state VARCHAR(30) NOT NULL,
+ zip INTEGER(10),
+ PRIMARY KEY(id)
+ );
