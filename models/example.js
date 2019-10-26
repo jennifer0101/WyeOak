89a7f4ee46cb5b593d334e9713c1d8c2cffda779
JeanCha <51973809+JeanCha@users.noreply.github.com>
2019-10-26 12:13:23 -0500

    update db with Name

--- models/example.js
+++ models/example.js
@@ -2,6 +2,7 @@ module.exports = function(sequelize, DataTypes) {
   var destinations = sequelize.define("Destinations", {
     //name: DataTypes.STRING,
     //address: DataTypes.STRING,
+    name: DataTypes.STRING,
     city: DataTypes.STRING,
     state: DataTypes.STRING
   });
