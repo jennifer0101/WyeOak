53642d7f0e9d30dfe3ce9e694d00cdc6f44e5994
connander <connorpstark@gmail.com>
2019-10-28 18:11:28 -0500

    google maps api working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,20 +5,6 @@ module.exports = function(app) {
   //then grabs the info
   //.then() res.json and put the call inside the function istelf instead of handlebars
 
-  app.post("/api/results/:name", function(req, res) {
-    db.wyeoak
-      .findOne({
-        where: {
-          name: req.params.name
-        },
-        attributes: ["city", "state"]
-      })
-      .then(function(results) {
-        var googleCall =
-          "https://www.google.com/maps/embed/v1/place?key={{google}}&q=" +
-          res.json(results);
-      });
-  });
   const axios = require("axios");
 
   app.post("/api/all", function(req, res) {
@@ -61,16 +47,20 @@ module.exports = function(app) {
 
  */
   });
-  /*
-    app.get("/api/all", function(req, res) {
 
-        //console.log(temp[0].data);
-        //return res.json(temp[0]);
-        /*
-        db.Destination.findAll({}).then(function(results) {
-            // results are available to us inside the .then
-            res.json(results);
-        });
+  app.get("api/results", function(req, res) {
+    //console.log(temp[0].data);
+    //return res.json(temp[0]);
 
-         */
+    db.Destinations.findOne({
+      where: {
+        id: 1
+      },
+      attributes: ["label"]
+    }).then(function(results) {
+      // results are available to us inside the .then
+      console.log(results);
+      res.json(results);
+    });
+  });
 };
